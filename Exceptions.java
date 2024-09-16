e917a60364e43ec194967840e2ea91af8ce9967f
Vishal101001 <126702524+Vishal101001@users.noreply.github.com>
2024-09-16 19:05:30 +0530

    Add files via upload
    exception methods

---
+++ Exceptions.java
@@ -0,0 +1,40 @@
+package com.createExceptions;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		
+		//____________________toString()________________
+		
+//		String s= "abc";
+//		try {
+//			System.out.println(s.toString());
+//		}
+//		catch(Exception e) {
+//			e.toString();
+//		}
+		
+		//_______________________printStackTrace()________________
+		//gives name and description
+		
+//		int a=5;
+//		int b=0;
+//		try {
+//			System.out.println(a/b);
+//		}
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		//__________________________getMessage()________________
+		
+//		int a=5;
+//		int b=0;
+//		try {
+//			System.out.println(a/b);
+//		}
+//		catch(Exception e) {
+//			e.getMessage();
+//		}
+	}
+}
